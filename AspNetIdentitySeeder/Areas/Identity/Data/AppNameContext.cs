458b249c24b67cf6173e9749fa68649d066ae945
pownas <pownas@outlook.com>
2021-01-01 10:45:31 +0100

    Lagt till Identity via Scaffolding
    Navigerat in under -> Areas -> Identity, högerklickar sedan på Identity och tar "Add -> New Scaffolded item" , därefter tar man Identity och Identity samt Add.
    
    Därefter ska ett Databas context och applikationsklass skapas för denna appen.
    Se bilder för detta.

---
+++ AspNetIdentitySeeder/Areas/Identity/Data/AppNameContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetIdentitySeeder.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetIdentitySeeder.Data
+{
+    public class AppNameContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppNameContext(DbContextOptions<AppNameContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ AspNetIdentitySeeder/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AspNetIdentitySeeder.Areas.Identity.Data;
+using AspNetIdentitySeeder.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AspNetIdentitySeeder.Areas.Identity.IdentityHostingStartup))]
+namespace AspNetIdentitySeeder.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AppNameContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AppNameContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AppNameContext>();
+            });
+        }
+    }
+}
